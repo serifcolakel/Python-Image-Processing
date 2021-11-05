381d2346792fa42d154cc732440ab3d3afd6121a
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-11-06 02:34:10 +0300

    keyboardOpenCV

--- keyboardOpenCV/main.py
+++ keyboardOpenCV/main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+s
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
