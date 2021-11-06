cb3629c76855cca40634d4f6caff892bfa713f6a
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-11-07 00:35:49 +0300

    AI keyBoard openCV kullarak sonuclandırıldı

--- keyboardOpenCV/README.md
+++ keyboardOpenCV/README.md
@@ -5,3 +5,10 @@
 * **pynput@1.7.3** for keyboard press
 # ACIKLAMA
 > mediapipe ile hand detect edilir ve mediapipe dökümanı içerisinde bulunan el indexlerinden 8(işaret parmağı ucu) ile 12(orta parmak ucu) arasındaki mesafeye göre pynput kullanılarak keyboard yada mouse de basma işlemi yapılabilir.
+## Referans Noktalarının tanıtılması
+![Image of Onay](https://i.hizliresim.com/c2rzqzj.PNG)
+## Referans Noktası ile Keyboard üzerinde bulunan harfin işaretlenmesi
+![Image of isaret](https://i.hizliresim.com/574o7hi.png)
+## Referans Noktası ile Karar Noktasının birleşmesi ile harfin Text: kısmına yazılması
+![Image of Detect](https://i.hizliresim.com/8ttoleg.png)
+## Durdumak icin Q tusuna basılması yeterli olacaktır.
--- keyboardOpenCV/main.py
+++ keyboardOpenCV/main.py
@@ -17,7 +17,7 @@ def drawAll(img, buttonList):
 
 
 class Button():
-    def __init__(self, pos, text, size=[85, 85]):
+    def __init__(self, pos, text, size=[90, 90]):
         self.pos = pos
         self.size = size
         self.text = text
@@ -32,13 +32,14 @@ detector = HandDetector(detectionCon=0.8)
 keys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "I", "O", "P"],
     ["A", " S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Z", "X", "X", "C", "V", "B", "N", "M", "/", "."]
+    ["Z", "X", "X", "C", "V", "B", "N", "M", "/", "."],
+    ["https://github.com/serifcolakel"]
     ]
 
 buttonList = []
 for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
-        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+        buttonList.append(Button([100 * j + 10  , 100 * i + 200 ], key))
 
 while True:
     success, img = cap.read()
@@ -51,11 +52,11 @@ while True:
             x, y = button.pos
             w, h = button.size
             #bas parmak ucu indexi
-            if x<lmlist[8][0]<x+w and y<lmlist[8][1]<y+h:
+            if x<lmlist[5][0]<x+w and y<lmlist[5][1]<y+h:
                 cv2.rectangle(img, button.pos, (x + w, y + h), (0, 0, 255), cv2.FILLED)
                 cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                 #isaret ve orta parmak indexleri mesafesi hesaplanacak ve l ye yazılacak.
-                l, _, _ = detector.findDistance(8, 12, img, draw=False)
+                l, _, _ = detector.findDistance(5, 8, img, draw=False)
                 #print(l)
                 #distance kucuk 30 ise tıklandıgında
                 if l<30:
@@ -63,13 +64,13 @@ while True:
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                     finalText += button.text
-                    sleep(0.15)
+                    sleep(3)
 
-    cv2.rectangle(img, (50, 350), (1280, 450), (175, 255, 175), cv2.FILLED)
-    cv2.putText(img, finalText, (60, 430), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    cv2.rectangle(img, (10, 1), (1280, 31), (0, 0, 0), cv2.FILLED)
+    cv2.putText(img, "Text : "+finalText, (60, 26), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
 
-    cv2.rectangle(img, (50, 500), (1280, 600), (175, 255, 175), cv2.FILLED)
-    cv2.putText(img, "KAPATMAK ICIN Q YA BAS", (60, 580), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    cv2.rectangle(img, (10, 690), (1280, 720), (255, 255, 0), cv2.FILLED)
+    cv2.putText(img, "KAPATMAK ICIN Q YA BAS", (60, 715), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
 
     cv2.imshow("image", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
