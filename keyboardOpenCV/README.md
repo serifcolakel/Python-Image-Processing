3d50a7574d1ec437abc191b21bd6de3c32745edb
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-11-06 23:03:18 +0300

    AI keyBoard openCV kullarak olusutruldu

---
+++ keyboardOpenCV/README.md
@@ -0,0 +1,7 @@
+# Kurulan Paketler
+* **Python@3.7.x**
+* **cvzone@1.4.1** for hand detect
+* **mediapipe@0.8.7** for hand detect
+* **pynput@1.7.3** for keyboard press
+# ACIKLAMA
+> mediapipe ile hand detect edilir ve mediapipe dökümanı içerisinde bulunan el indexlerinden 8(işaret parmağı ucu) ile 12(orta parmak ucu) arasındaki mesafeye göre pynput kullanılarak keyboard yada mouse de basma işlemi yapılabilir.
--- keyboardOpenCV/main.py
+++ keyboardOpenCV/main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+from pynput.keyboard import Controller
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    return img
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+finalText = ""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+keyboard = Controller()
+
+#Track Hand
+detector = HandDetector(detectionCon=0.8)
+keys = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "I", "O", "P"],
+    ["A", " S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Z", "X", "X", "C", "V", "B", "N", "M", "/", "."]
+    ]
+
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist, bboxInfo = detector.findPosition(img)
+    img = drawAll(img, buttonList)
+    #hand check
+    if lmlist:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+            #bas parmak ucu indexi
+            if x<lmlist[8][0]<x+w and y<lmlist[8][1]<y+h:
+                cv2.rectangle(img, button.pos, (x + w, y + h), (0, 0, 255), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                #isaret ve orta parmak indexleri mesafesi hesaplanacak ve l ye yazılacak.
+                l, _, _ = detector.findDistance(8, 12, img, draw=False)
+                #print(l)
+                #distance kucuk 30 ise tıklandıgında
+                if l<30:
+                    keyboard.press(button.text)#button.text'e basacak
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                    finalText += button.text
+                    sleep(0.15)
+
+    cv2.rectangle(img, (50, 350), (1280, 450), (175, 255, 175), cv2.FILLED)
+    cv2.putText(img, finalText, (60, 430), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.rectangle(img, (50, 500), (1280, 600), (175, 255, 175), cv2.FILLED)
+    cv2.putText(img, "KAPATMAK ICIN Q YA BAS", (60, 580), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.imshow("image", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
